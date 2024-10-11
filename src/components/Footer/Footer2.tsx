a596abfae3d4f09317211198566b62ee7d610bbd
sumit <sk7627577@gmail.com>
2024-10-11 13:58:40 +0530

    Privacy Policy content added

--- src/components/Footer/Footer2.tsx
+++ src/components/Footer/Footer2.tsx
@@ -165,7 +165,7 @@ function Footer2() {
           </div>
 
           <div className="flex md:flex-row flex-col max-md:items-center gap-4 text-gray-primary justify-between text-xl max-md:mt-5 mt-10">
-            <Link href={"/"}>Privacy Policy</Link>
+            <Link href={"/privacy-policy"}>Privacy Policy</Link>
             <Link href={"/terms-and-conditions/"}>Terms & Conditions</Link>
           </div>
         </div>
