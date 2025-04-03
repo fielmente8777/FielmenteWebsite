2e21c540d6775b997835621e702b90f1689b6587
sumit <sk7627577@gmail.com>
2025-04-03 14:32:10 +0530

    feat: update Next.js configuration to enable export output and adjust image settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    /* config options here */
-  // output: "export",
+  output: "export",
   images: {
-    // unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.builder.io",
-      },
-    ],
-    dangerouslyAllowSVG: true,
+    unoptimized: true,
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "cdn.builder.io",
+    //   },
+    // ],
+    // dangerouslyAllowSVG: true,
   },
   trailingSlash: true,
 };
