60a4dad18fd23b9961c9d841dae17d6ea1bff32f
Hem Bhadur <sushilkc2611@gmail.com>
2025-06-18 13:20:59 +0530

    implemented chatbot tq page

--- src/components/chatbot/RenderChatBot.tsx
+++ src/components/chatbot/RenderChatBot.tsx
@@ -102,7 +102,9 @@ const RenderChatBot = () => {
           },
         }
       );
-      console.log(data);
+      if (data?.Status) {
+        window.open("thank-you", "_blank");
+      }
     } catch (error) {
       console.log(error);
     }
