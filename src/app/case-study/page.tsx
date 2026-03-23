a2cdb97c0a4378d33cbeb1bbb0490ed2ec5a9a1c
Personal <sk7627577@gmail.com>
2026-03-23 21:38:54 +0530

    up

--- src/app/case-study/page.tsx
+++ src/app/case-study/page.tsx
@@ -4,6 +4,7 @@ import { homePageData } from "../(home)/homePageData";
 import Link from "next/link";
 import { casStudyData } from "./caseData";
 import CasStudyCard from "./components/CasStudyCard";
+import { NextBtnIcon } from "./components/icon";
 
 export default function CaseStudy() {
   return (
@@ -38,17 +39,4 @@ export default function CaseStudy() {
   );
 }
 
-export const NextBtnIcon = () => (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M13.5252 12.003L8.21026 17.7106C8.07009 17.8611 8.00176 18.0384 8.00529 18.2423C8.00894 18.4464 8.08085 18.6237 8.22102 18.7742C8.36131 18.9247 8.52642 19 8.71633 19C8.90625 19 9.07136 18.9247 9.21165 18.7742L14.6241 12.9734C14.7517 12.8364 14.8462 12.6829 14.9077 12.5129C14.9692 12.343 15 12.173 15 12.003C15 11.833 14.9692 11.6631 14.9077 11.4931C14.8462 11.3231 14.7517 11.1696 14.6241 11.0326L9.21165 5.22007C9.07135 5.06954 8.90443 4.99623 8.71087 5.00015C8.5173 5.00406 8.35043 5.08129 8.21026 5.23182C8.07009 5.38235 8 5.55965 8 5.76372C8 5.96767 8.07009 6.14491 8.21026 6.29544L13.5252 12.003Z"
-      fill="currentColor"
-    />
-  </svg>
-);
+
